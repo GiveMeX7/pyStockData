efdae292319f0288d96ec5f1f74bbe26eafdf4a7
Sam Ho <samho5888@gmail.com>
2014-09-03 18:03:27 +0800

    0903

--- src/fetch7.py
+++ src/fetch7.py
@@ -33,17 +33,32 @@ class fetch7:
 		csvFile = 'http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX3_print.php?genpage=genpage/Report'
 		csvFile += str(today.year) + str(today.month).zfill(2) + '/A112'
 		csvFile += str(today.year) + str(today.month).zfill(2) + str(today.day).zfill(2) + 'ALL_1.php&type=csv'
-		print csvFile
+
+		#print csvFile
+		csvFile = 'http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX3_print.php?genpage=genpage/Report201408/A11220140826ALL_1.php&type=csv'
 
 		the_page = urllib.urlopen(csvFile).read()
 		if len(the_page) == 0:
-			print("Go no page\n")
+			print("Got no page\n")
 			#continue
 
 		uthe_page = unicode(the_page, 'cp950')
 		# TODO: find 1101 to 漲跌符號說明
 
+		#
+		# 證券代號, 證券名稱, 成交股數,     成交筆數, 成交金額,      開盤價, 最高價, 最低價, 收盤價, 漲跌(+/-), 漲跌價差, 最後揭示買價, 最後揭示買量, 最後揭示賣價, 最後揭示賣量, 本益比
+		# 1101,     台泥,     "11,137,634", "4,308",  "539,339,150", 49.55,  49.55,  48.10,  48.30,  －,        1.25,     48.25,        62,           48.30,        130,          16.15
+		# ...
+		# 漲跌符號說明:[+ ->漲][- ->跌][X ->不比價]
+		urlData = string.split(uthe_page, u'1101')
+		if len(urlData) == 1:
+			exit()
+		urlData = u'1101' + urlData[1]
 
+		b = StringIO(urlData.encode('utf-8'))
+		r = csv.reader(b, delimiter=',', quotechar='"')
+		for row in r:
+		    print row
 
 #
 # Test case
